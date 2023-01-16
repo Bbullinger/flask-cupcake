0061605f513d74b3c4c4f97fee784b5d1bf8d549
Bbullinger <Brandon.Bullinger@Yahoo.com>
2023-01-16 14:51:08 -0500

    some progress with js

--- static/cupcake.js
+++ static/cupcake.js
@@ -3,6 +3,13 @@ const BASE_URL = "http://localhost:5000/api";
 // This will give us the HTML used to display each cupcake
 const cupcakeList = document.querySelector("#cupcakeList");
 
+//DOM elements related to form to add a new cupcake
+const cupcakeForm = document.querySelector("#cupcakeForm");
+const addFlavor = document.querySelector("cupcakeFlavor");
+const addRating = document.querySelector("cupcakeRating");
+const addSize = document.querySelector("cupcakeSize");
+const addImageURL = document.querySelector("cupcakeImageURL");
+
 function cupcakeHTML(cupcake) {
   const cupcakeDiv = document.createElement("div");
   cupcakeDiv.setAttribute("class", "cupcake");
@@ -21,7 +28,7 @@ function cupcakeHTML(cupcake) {
   cupcakeLi.append(cupcakePSize);
 
   const cupcakePRating = document.createElement("p");
-  cupcakePSize.innerText = `${cupcake.rating}/10`;
+  cupcakePRating.innerText = `${cupcake.rating}/10`;
   cupcakeLi.append(cupcakePRating);
 
   const cupcakeImage = document.createElement("img");
@@ -29,23 +36,18 @@ function cupcakeHTML(cupcake) {
   cupcakeImage.setAttribute("class", "cupcakeImage");
   cupcakeDiv.append(cupcakeImage);
 
-  console.log(
-    cupcakeDiv,
-    cupcakeLi,
-    cupcakePFlavor,
-    cupcakePSize,
-    cupcakePRating
-  );
-}
+  console.log(cupcake.size);
 
-//   return `<div class="cupcake" data-cupcake-id="cupcake.id">
-//     <li>
-//         <p>${cupcake.flavor}</p>
-//         <p>${cupcake.size}</p>
-//         <p>${cupcake.rating}</p>
-//     </li>
-//     <img class="cupcakeImage" src="cupcake.image_url">
-// </div>`;
+  //HTML should look like:
+  // `<div class="cupcake" data-cupcake-id="cupcake.id">
+  //     <li>
+  //         <p>${cupcake.flavor}</p>
+  //         <p>${cupcake.size}</p>
+  //         <p>${cupcake.rating}</p>
+  //     </li>
+  //     <img class="cupcakeImage" src="cupcake.image_url">
+  // </div>`;
+}
 
 async function showAllCupcakes() {
   const response = await axios.get(`${BASE_URL}/cupcakes`);
@@ -56,3 +58,24 @@ async function showAllCupcakes() {
 }
 
 showAllCupcakes();
+
+async function submitCupcakeForm(e) {
+  e.preventDefault();
+
+  //grab all values from cupcake form
+  const flavor = addFlavor.val();
+  const rating = addRating.val();
+  const size = addSize.val();
+  const imageURL = addImageURL.val();
+
+  const postCupcakeResp = await axios.post(`${BASE_URL}/cupcakes`, {
+    flavor,
+    rating,
+    size,
+    imageURL,
+  });
+  const newCupcake = cupcakeHTML(postCupcakeResp.data.cupcake);
+  console.log(newCupcake);
+}
+
+cupcakeForm.addEventListener("submit", submitCupCakeForm(e));
